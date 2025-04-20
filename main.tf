53196ea0e40f401cc7ae4a0d1b80d475e028a3da
abhixsh <aloka.abishek9@gmail.com>
2025-04-20 23:39:15 +0530

    Add Azure provider configuration and sensitive variables for authentication

--- main.tf
+++ main.tf
@@ -9,10 +9,20 @@ terraform {
 
   backend "azurerm" {
     resource_group_name  = "tfstate-mlsa"
-    storage_account_name = "tfstatestoragemlsa123"   
+    storage_account_name = "tfstatestoragemlsa123"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
+}
+
+provider "azurerm" {
+  features {}
+
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
 }
 
 # Configure Azure provider
