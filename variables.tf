d18829ae308349293d39eced3902f8ad4df8f660
abhixsh <aloka.abishek9@gmail.com>
2025-04-21 09:00:55 +0530

    Refactor Terraform configuration to clean up variable definitions and remove unused client credentials

--- variables.tf
+++ variables.tf
@@ -7,31 +7,11 @@ variable "location" {
 }
 
 variable "admin_username" {
-  type      = string
+  type = string
   sensitive = true
 }
 
 variable "admin_password" {
-  type      = string
-  sensitive = true
-}
-
-variable "client_id" {
-  type      = string
-  sensitive = true
-}
-
-variable "client_secret" {
-  type      = string
-  sensitive = true
-}
-
-variable "tenant_id" {
-  type      = string
-  sensitive = true
-}
-
-variable "subscription_id" {
-  type      = string
+  type = string
   sensitive = true
 }
